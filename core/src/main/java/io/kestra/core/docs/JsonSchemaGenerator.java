9b3e1a15e3888c32fd7bc8c4180497b73156d412
YannC. <ycoornaert@kestra.io>
2026-02-19 16:00:58 +0100

    feat: added the possibility to annotate a plugin or a plugin property with a language
    close #14187

--- core/src/main/java/io/kestra/core/docs/JsonSchemaGenerator.java
+++ core/src/main/java/io/kestra/core/docs/JsonSchemaGenerator.java
@@ -31,6 +31,7 @@ import io.kestra.core.models.dashboards.DataFilterKPI;
 import io.kestra.core.models.dashboards.charts.Chart;
 import io.kestra.core.models.dashboards.charts.DataChart;
 import io.kestra.core.models.dashboards.charts.DataChartKPI;
+import io.kestra.core.models.enums.MonacoLanguages;
 import io.kestra.core.models.property.Data;
 import io.kestra.core.models.property.Property;
 import io.kestra.core.models.tasks.Output;
@@ -52,7 +53,8 @@ import jakarta.inject.Singleton;
 import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.*;
-import java.time.*;
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -391,6 +393,9 @@ public class JsonSchemaGenerator {
                 if (pluginPropertyAnnotation.beta()) {
                     memberAttributes.put("$beta", true);
                 }
+                if (pluginPropertyAnnotation.language() != MonacoLanguages.NONE) {
+                    memberAttributes.put("$language", pluginPropertyAnnotation.language().toString());
+                }
                 if (pluginPropertyAnnotation.internalStorageURI()) {
                     memberAttributes.put("$internalStorageURI", true);
                 }
@@ -458,6 +463,10 @@ public class JsonSchemaGenerator {
 
                     if (pluginAnnotation.beta()) {
                         collectedTypeAttributes.put("$beta", true);
+                    }
+
+                    if (pluginAnnotation.language() != MonacoLanguages.NONE) {
+                        collectedTypeAttributes.put("$language", pluginAnnotation.language().toString());
                     }
 
                     if (withOutputs) {
@@ -952,6 +961,9 @@ public class JsonSchemaGenerator {
         }
         if (mainClassDef.has("$beta")) {
             objectNode.set("$beta", mainClassDef.get("$beta"));
+        }
+        if (mainClassDef.has("$language")) {
+            objectNode.set("$language", mainClassDef.get("$language"));
         }
     }
 
--- core/src/test/java/io/kestra/core/docs/JsonSchemaGeneratorTest.java
+++ core/src/test/java/io/kestra/core/docs/JsonSchemaGeneratorTest.java
@@ -7,6 +7,7 @@ import io.kestra.core.models.annotations.Plugin;
 import io.kestra.core.models.annotations.PluginProperty;
 import io.kestra.core.models.dashboards.Dashboard;
 import io.kestra.core.models.dashboards.GraphStyle;
+import io.kestra.core.models.enums.MonacoLanguages;
 import io.kestra.core.models.flows.Flow;
 import io.kestra.core.models.property.Property;
 import io.kestra.core.models.tasks.RunnableTask;
@@ -250,8 +251,10 @@ class JsonSchemaGeneratorTest {
         Map<String, Object> generate = jsonSchemaGenerator.properties(Task.class, BetaTask.class);
         assertThat(generate, is(not(nullValue())));
         assertThat(generate.get("$beta"), is(true));
+        assertThat(generate.get("$language"), is(MonacoLanguages.JAVASCRIPT.toString()));
         assertThat(((Map<String, Map<String, Object>>) generate.get("properties")).size(), is(2));
         assertThat(((Map<String, Map<String, Object>>) generate.get("properties")).get("beta").get("$beta"), is(true));
+        assertThat(((Map<String, Map<String, Object>>) generate.get("properties")).get("beta").get("$language"), is(MonacoLanguages.PYTHON.toString()));
     }
 
     @SuppressWarnings("unchecked")
@@ -445,10 +448,11 @@ class JsonSchemaGeneratorTest {
     @Getter
     @NoArgsConstructor
     @Plugin(
-        beta = true
+        beta = true,
+        language = MonacoLanguages.JAVASCRIPT
     )
     public static class BetaTask extends Task {
-        @PluginProperty(beta = true)
+        @PluginProperty(beta = true, language = MonacoLanguages.PYTHON)
         private String beta;
     }
 
